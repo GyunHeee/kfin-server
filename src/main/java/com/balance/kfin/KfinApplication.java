0271c6b8b6b124003091008946f5354681bd1ac2
GyunHee <ef3232@naver.com>
2026-04-27 18:49:34 +0900

    init: init project

---
+++ src/main/java/com/balance/kfin/KfinApplication.java
@@ -0,0 +1,13 @@
+package com.balance.kfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balance/kfin/KfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balance.kfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
